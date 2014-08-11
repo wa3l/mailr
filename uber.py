f2290a230990ad858df010a8255dd4ac8d37ea47
Wael Al-Sallami <wael.alsallami@gmail.com>
2014-08-11 18:12:53 -0500

    added a test email endpoint

--- uber.py
+++ uber.py
@@ -1,8 +1,22 @@
 import os
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
   return "this is a test index page"
+
+
+@app.route('/email', methods=['GET', 'POST'])
+def email():
+  assert request.method == 'POST'
+  # this is a test email page
+  return request.form['body']
+
+
+if __name__ == '__main__':
+  app.run()
+
