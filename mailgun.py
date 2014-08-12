585c375b9779df4cdcd4f49f0138d7dd2d4c1232
Wael Al-Sallami <wael.alsallami@gmail.com>
2014-08-11 20:59:39 -0500

    built a wrapper around Mailgun. Works but needs testing and error checking

---
+++ mailgun.py
@@ -0,0 +1,52 @@
+from api_keys import APIKeys
+import urllib2, urllib
+
+class Mailgun():
+  """
+  Wrapper for Mailgun
+
+  Automatically handles creating a request to the Mailgun service
+  provided a email object that contains email data.
+  """
+
+  def __init__(self):
+    """
+    Initiate the object with the proper API keys.
+    """
+    self.key = APIKeys.MAILGUN_KEY
+    self.url = APIKeys.MAILGUN_URL
+
+
+  def authenticate(self):
+    """
+    Authenticate to the Mailgun service using our API keys.
+    This is ugly code due to urllib's low levelness.
+    """
+    password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_manager.add_password(None,
+                                  APIKeys.MAILGUN_URL,
+                                  'api',
+                                  APIKeys.MAILGUN_KEY
+                                  )
+    # build a basic HTTP auth handler
+    # build an opener with it and finally install it:
+    auth   = urllib2.HTTPBasicAuthHandler(password_manager)
+    opener = urllib2.build_opener(auth)
+    urllib2.install_opener(opener)
+
+
+  def send(self, email):
+    """
+    Send the email using the provided email dict.
+    """
+    self.authenticate()
+    data =  {
+              "from": "{0} <{1}>".format(email['from_name'], email['from']),
+              "to":   "{0} <{1}>".format(email['to_name'],   email['to']),
+              "subject": email['subject'],
+              "text": email['text'],
+              "html": email['html']
+            }
+    request = urllib2.Request(APIKeys.MAILGUN_URL, urllib.urlencode(data))
+    handler = urllib2.urlopen(request)
+    return handler.read()
--- uber.py
+++ uber.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask import request
+from mailgun import Mailgun
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,11 +13,20 @@ def index():
 
 @app.route('/email', methods=['GET', 'POST'])
 def email():
-  assert request.method == 'POST'
-  # this is a test email page
-  return request.form['body']
+  # test email service:
+  email = {
+    "to": "walsallami@gmail.com",
+    "to_name": "Wael Al-Sallami",
+    "from": "wael.alsallami@gmail.com",
+    "from_name": "Katie Lea",
+    "subject": "A Message from Uber",
+    "text": "Your Bill: $10",
+    "html": "<h1>Your Bill</h1><p>$10</p>"
+  }
+  m = Mailgun()
+  response = m.send(email)
+  return response
 
 
 if __name__ == '__main__':
   app.run()
-
