a8e5a09652e7c06005a214cdf48f55a723782ca0
Wael Al-Sallami <wael.alsallami@gmail.com>
2014-08-11 21:33:41 -0500

    refactored some of the mailgun wrapper

--- mailgun.py
+++ mailgun.py
@@ -20,12 +20,13 @@ class Mailgun():
   def authenticate(self):
     """
     Authenticate to the Mailgun service using our API keys.
-    This is ugly code due to urllib's low levelness.
+    This is ugly code due to urllib's low levelness. It:
+      1. Creates a password manager and configures it.
+      2. Builds a Basic HTTP Auth handler.
+      3. Builds an opener and instalsl it.
     """
     password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
     password_manager.add_password(None, self.url, 'api', self.key)
-    # build a basic HTTP auth handler
-    # build an opener with it and finally install it:
     auth   = urllib2.HTTPBasicAuthHandler(password_manager)
     opener = urllib2.build_opener(auth)
     urllib2.install_opener(opener)
@@ -34,6 +35,7 @@ class Mailgun():
   def send(self, email):
     """
     Send the email using the provided email dict.
+    First authenticate to Mailgun, then build the request object and send it
     """
     self.authenticate()
     data =  {
